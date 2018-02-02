00e67c581a46ceec0c40ab0bf71faf2c6bf1810c
KokiHirokawa <kokihirokawa1110@icloud.com>
2018-02-02 17:03:35 +0900

    add mapStyle

--- kaivap/ViewController.swift
+++ kaivap/ViewController.swift
@@ -19,46 +19,322 @@ extension UIColor {
         let b = CGFloat(Int(v[4] + v[5], radix: 16) ?? 0) / 255.0
         self.init(red: r, green: g, blue: b, alpha: alpha)
     }
-    
+
     convenience init(hex: String) {
         self.init(hex: hex, alpha: 1.0)
     }
 }
 
 class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate {
-    
+
     var locationManager: CLLocationManager!
 //    var currentLocation: CLLocation?
     var currentCameraPosition: GMSCameraPosition?
     var zoomLevel: Float = 15.0
     var mapView: GMSMapView!
-    
+
+    let mapStyle = "[" +
+"    {" +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#ebe3cd\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"elementType\": \"labels.text.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#523735\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"elementType\": \"labels.text.stroke\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#f5f1e6\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"administrative\"," +
+"    \"elementType\": \"geometry.stroke\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#c9b2a6\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"administrative.land_parcel\"," +
+"    \"elementType\": \"geometry.stroke\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#dcd2be\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"administrative.land_parcel\"," +
+"    \"elementType\": \"labels\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"visibility\": \"off\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"administrative.land_parcel\"," +
+"    \"elementType\": \"labels.text.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#ae9e90\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"landscape.natural\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#dfd2ae\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"poi\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#dfd2ae\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"poi\"," +
+"    \"elementType\": \"labels.text.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#93817c\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"poi.business\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"visibility\": \"off\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"poi.park\"," +
+"    \"elementType\": \"geometry.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#a5b076\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"poi.park\"," +
+"    \"elementType\": \"labels.text\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"visibility\": \"off\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"poi.park\"," +
+"    \"elementType\": \"labels.text.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#447530\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#f5f1e6\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.arterial\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"visibility\": \"off\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.arterial\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#fdfcf8\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.highway\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#f8c967\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.highway\"," +
+"    \"elementType\": \"geometry.stroke\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#e9bc62\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.highway\"," +
+"    \"elementType\": \"labels\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"visibility\": \"off\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.highway.controlled_access\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#e98d58\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.highway.controlled_access\"," +
+"    \"elementType\": \"geometry.stroke\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#db8555\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.local\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"visibility\": \"off\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.local\"," +
+"    \"elementType\": \"labels\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"visibility\": \"off\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"road.local\"," +
+"    \"elementType\": \"labels.text.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#806b63\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"transit.line\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#dfd2ae\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"transit.line\"," +
+"    \"elementType\": \"labels.text.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#8f7d77\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"transit.line\"," +
+"    \"elementType\": \"labels.text.stroke\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#ebe3cd\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"transit.station\"," +
+"    \"elementType\": \"geometry\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#dfd2ae\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"water\"," +
+"    \"elementType\": \"geometry.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#b9d3c2\"" +
+"    }" +
+"    ]" +
+"    }," +
+"    {" +
+"    \"featureType\": \"water\"," +
+"    \"elementType\": \"labels.text.fill\"," +
+"    \"stylers\": [" +
+"    {" +
+"    \"color\": \"#92998d\"" +
+"    }" +
+"    ]" +
+"    }" +
+"    ]"
+
     @IBOutlet weak var elevationLabel: UILabel!
     @IBOutlet weak var calculateButton: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
-    
+
     func setupView() {
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
         let mapViewSize = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height-88)
         mapView = GMSMapView.map(withFrame: mapViewSize, camera: camera)
         mapView.isMyLocationEnabled = true
         mapView.delegate = self
-        
+
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
         marker.map = mapView
         view.addSubview(mapView)
-        
+
         calculateButton.layer.borderWidth = 1.0
         calculateButton.layer.borderColor = UIColor(hex: "dadada").cgColor
         calculateButton.layer.cornerRadius = 22
-        
+
         locationManager = CLLocationManager()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
@@ -66,14 +342,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
         locationManager.startUpdatingLocation()
         locationManager.delegate = self
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location: CLLocation = locations.last!
-        
+
         let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
                                               longitude: location.coordinate.longitude,
                                               zoom: zoomLevel)
-        
+
         if mapView.isHidden {
             mapView.isHidden = false
             mapView.camera = camera
@@ -90,7 +366,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
             }
         }
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         switch status {
         case .restricted:
@@ -105,18 +381,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
             print("Location status is OK.")
         }
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         locationManager.stopUpdatingLocation()
         print("Error: \(error)")
     }
-    
+
     func mapView(_ mapView: GMSMapView, didChange position: GMSCameraPosition) {
         currentCameraPosition = position
     }
-    
+
     @IBAction func didPressCalculateButton(_ sender: Any) {
-        
+
         if let currentCameraPosition = currentCameraPosition {
             let request = GetElevationRequest(lat: currentCameraPosition.target.latitude, lng: currentCameraPosition.target.longitude)
             Session.send(request) { result in
@@ -129,9 +405,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
             }
         }
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 }
-
